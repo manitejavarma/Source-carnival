523efadb3c0e54c37a783c355ba15124ed18176c
manitejavarma <manitejavarma@yahoo.com>
2015-09-01 16:55:28 +0530

    database 1.0

--- db.js
+++ db.js
@@ -13,19 +13,35 @@ var pool = m.createPool({
         password: 'akhil',
 });
 
-module.exports.login = function() {
+module.exports.login = function(opt) {
         pool.getConnection(function(err, conn) {
-                conn.query("", function(err, rows) {
+                queryString='select * from students where (email="+opt.email+"or username="+opt.username+")password="+opt.password'
+                conn.query(queryString, function(err, rows) {
                         conn.close();
                 });
         });
 }
 
-module.exports.deleteAccount = function() {
+module.exports.deleteAccount = function(opt) {
         pool.getConnection(function(err, conn) {
-                conn.query("", function(err, rows) {
+		queryString='delete * from students where email='+opt.email;
+                conn.query(queryString, function(err, rows) {
                         conn.close();
                 });
         });
 }
+
+module.exports.forgotPassword = function(opt) {
+        pool.getConnection(function(err, conn) {
+                queryString='select password from students where email='opt.email;
+                conn.query(queryString, function(err, rows) {
+                        conn.close();
+                });
+        });
+}
+
+
+
+
+
 
